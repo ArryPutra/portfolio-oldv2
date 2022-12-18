58a3de6f24b57a74c5e93da8bcd8272249d0e2bf
ArryPutra <113241887+ArryPutra@users.noreply.github.com>
2022-12-18 21:23:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// DARKMODE
+let darkMode = document.querySelector("header");
+let buttonDarkMode = document.querySelector(".dark-mode");
+
+buttonDarkMode.onclick = function() {
+    darkMode.classList.toggle("darkMode");
+}
+
+// MENU MOBILE
+let menuMobile = document.querySelector(".menu-mobile");
+
